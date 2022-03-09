74f1da59d0f7c5f5f9fde64da255291fe11276b6
Aakash109 <98142600+Aakash109@users.noreply.github.com>
2022-03-09 19:24:09 +0530

    ass1

--- ass_1.c
+++ ass_1.c
@@ -7,17 +7,16 @@ void main ()
 int f1,f2,len,total;
 char buff_copy[100];
 char new[100];
-f1=open("hello.txt",O_RDONLY,777);
+f1=open("linuxxxyz.txt",O_RDONLY,777);
 
 read(f1,buff_copy,100);
 total=lseek(f1,0,SEEK_END);
 close(f1);
 printf("total=%d \n",total);
 
-f2=open("new1.txt",O_RDWR|O_CREAT,777);
+f2=open("ass.txt",O_RDWR|O_CREAT,777);
 write(f2,buff_copy,total);
-read(f2,new,total);
-printf("the data=%s\n",new);
+printf("the data=%s\n",buff_copy);
 
 close(f2);
 
